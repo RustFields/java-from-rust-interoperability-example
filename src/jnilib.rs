9ae4fefb69988fe77bb62b81696d420a4a3f46a4
Filippo Vissani <filippo.vissani@studio.unibo.it>
2023-04-09 09:02:11 +0200

    feat: restructured project

--- src/jnilib.rs
+++ src/jnilib.rs
@@ -1,11 +1,10 @@
-use jni::{InitArgsBuilder, JavaVM, JNIVersion};
 use jni::errors::StartJvmError;
+use jni::{InitArgsBuilder, JavaVM, JNIVersion};
 use jni::objects::JValue;
 use jni::sys::jint;
 
-#[test]
-fn test_jvm() -> Result<(), StartJvmError> {
-// Build the VM properties
+pub fn abs(i: i32) -> Result<jint, StartJvmError> {
+    // Build the VM properties
     let jvm_args = InitArgsBuilder::new()
         // Pass the JNI API version (default is 8)
         .version(JNIVersion::V8)
@@ -15,7 +14,6 @@ fn test_jvm() -> Result<(), StartJvmError> {
         .option("-Xcheck:jni")
         .build()
         .unwrap();
-
 // Create a new VM
     let jvm = JavaVM::new(jvm_args)?;
 
@@ -25,11 +23,17 @@ fn test_jvm() -> Result<(), StartJvmError> {
 // This method returns the guard that will detach the current thread when dropped,
 // also freeing any local references created in it
     let mut env = jvm.attach_current_thread()?;
-
 // Call Java Math#abs(-10)
-    let x = JValue::from(-10);
+    let x = JValue::from(i);
     let val: jint = env.call_static_method("java/lang/Math", "abs", "(I)I", &[x])?.i()?;
+    Ok(val)
+}
 
-    assert_eq!(val, 10);
-    Ok(())
+#[cfg(test)]
+mod tests{
+    use crate::jnilib::abs;
+    #[test]
+    fn test_abs() {
+        assert_eq!(abs(-10).unwrap(), 10);
+    }
 }
